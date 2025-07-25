5ac176be2432554f3e534c136b3a38b2ef5c1dae
thatzcool <mycaptain0771@gmail.com>
2025-07-25 17:19:54 +0900

    operator_

--- src/day03/OperatorEx01.java
+++ src/day03/OperatorEx01.java
@@ -54,10 +54,50 @@ public class OperatorEx01 {
 
         System.out.println(y1);
 
+    // 산술연산자  +, - , * , / , %(나머지)
+          byte v1 = 10;
+          byte v2 = 4;
+          int v3 = 5;
+          long v4 = 10L;
+
+        int result4 = v1 + v2;    //모든 피연산자는 int타입으로 자동변환 후 연산
+        System.out.println(result4);
+        long result5 = v1 + v2 -v4;  //모든 피연산자는 long 타입으로 promotion 연산
+        System.out.println(result5);
+        double result6 = (double)v1/v2;   //double로 타입 강제변환 후 연산
+        System.out.println(result6);
+
+        int result7 = v1 % v2;
+        System.out.println(result7);
+
+        if(10 % 2 == 0) {
+            System.out.println("짝수 입니다.");
+        } else {
+            System.out.println("");
+        }
 
 
+        int apple = 1;
+        int totalPieces = apple * 10;
+        int number = 7 ;
+
+        int result = totalPieces - number;
+        System.out.println("10조각에서 남은 조각 : " + result);
+        System.out.println("사과 1개에서 남은 양 : " + result/10.0);
+
+
+        int k = 5;
+        int result8 = k/2;
+        System.out.println(result8);
 
     }
+
+    //비교 연산자 동등( == , !=) , 크기( <, <= , >, >=) 를 평가해서 boolean타입인 true, false 산출한다.
+
+
+
+
+
 
 
 }
---
+++ src/day03/OperatorEx04.java
@@ -0,0 +1,33 @@
+package day03;
+
+public class OperatorEx04 {
+    public static void main(String[] args) {
+
+        // & bit 단위로 논리연산을 수행한다.   대상은 bit 0 , 1
+        // 피연산자가 2진수 0 과 1 로 저장되는 정수타입(byte,short,int,long)만 대상이 된다.
+        // 실수타입(float , double) bit연산의 대상이 될 수 없다.
+
+        int num1 = 1;
+        int result1 = num1 << 3;
+        //Math.pow(2,3) => 2의 3제곱 =>
+        int result2 = num1 * (int) Math.pow(2, 3);
+        System.out.println("result1 :" + result1);
+        System.out.println("result2 :" + result2);
+
+        //대입연산자    , 복합대입연산자  +=  -=   *=   /=  %=
+
+        int r = 0;
+        r += 10;    //  r = r + 10;
+        System.out.printf("현재 r의 값  %d: ", r);
+        r -= 5;    // r = r - 5;
+        System.out.printf("현재 r의 값  %d: ", r);
+        r *= 2;    // r = r*2;
+        System.out.printf("현재 r의 값  %d: ", r);
+        r /= 5;    // r = r / 5;
+        System.out.printf("현재 r의 값  %d: ", r);
+        r %= 2;    // r = r % 2;
+        System.out.printf("현재 r의 값  %d: ", r);
+
+    }
+
+}
