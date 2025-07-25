89b3e6cf43f322b5687b0614923ee1811c7c419a
thatzcool <mycaptain0771@gmail.com>
2025-07-25 17:30:01 +0900

    operator_end

---
+++ src/day03/OperatorEx05.java
@@ -0,0 +1,26 @@
+package day03;
+
+import java.util.Scanner;
+
+public class OperatorEx05 {
+    public static void main(String[] args) {
+        //삼항 연산자 (피연산자 ? 피연산자 : 피연산자)
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("국어 점수를 입력하세요!");
+        int kor = sc.nextInt();
+
+        char grade = (kor > 90) ? 'A'  : (kor>80)? 'B' :'C';
+
+        System.out.println(grade);
+
+        if(kor > 90){
+            System.out.println('A');
+        }else if(kor > 80){
+            System.out.println('B');
+        }else {
+            System.out.println('C');
+        }
+
+    }
+}
