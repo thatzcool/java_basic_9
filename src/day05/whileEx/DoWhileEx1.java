ef93be067fb10f846988423875ffb6e7ed74450a
thatzcool <mycaptain0771@gmail.com>
2025-07-29 12:39:32 +0900

    controlstatement_end

---
+++ src/day05/whileEx/DoWhileEx1.java
@@ -0,0 +1,20 @@
+package day05.whileEx;
+
+import java.util.Scanner;
+
+public class DoWhileEx1 {
+    public static void main(String[] args) {
+
+          int x = 10;
+          int sum = 0;
+
+          do {
+              System.out.println("현재 x :" + x);
+              sum += x;
+              x--;
+              System.out.println("처리 후 x, sum : " + x + " " + sum);
+          } while(x >=5);
+        System.out.println("-----------------------");
+        System.out.println("최종 sum : " + sum);
+    }
+}
