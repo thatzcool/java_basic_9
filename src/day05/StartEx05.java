87366cd095681f92431218cf51f93c7ccba99ba6
thatzcool <mycaptain0771@gmail.com>
2025-07-29 10:57:01 +0900

    for_start_02

---
+++ src/day05/StartEx05.java
@@ -0,0 +1,14 @@
+package day05;
+
+public class StartEx04 {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j > i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
