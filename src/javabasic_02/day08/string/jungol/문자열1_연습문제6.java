d126b52363f77666cb5281b8a49be1358ff2a166
thatzcool <mycaptain0771@gmail.com>
2025-08-01 15:18:25 +0900

    jungol_string1_06_1

--- src/javabasic_02/day08/string/jungol/문자열1_연습문제6.java
+++ src/javabasic_02/day08/string/jungol/문자열1_연습문제6.java
@@ -14,16 +14,29 @@ public class 문자열1_연습문제6 {
             String str = in.nextLine();
             char ch = str.charAt(0);
 
-            if ((int) ch >= 123 || (int) ch <= 47) {
+//            if ((int) ch >= 123 || (int) ch <= 47) {
+//                System.out.println("영문, 숫자 이외의 문자입니다.");
+//                break;
+//            }
+//
+//            if (isDigit(ch)) {
+//                System.out.println("숫자문자입니다.");
+//            } else {
+//                System.out.println(isUpperCase(ch) ? "대문자입니다." : "소문자입니다.");
+//         }
+
+            if (ch >= 'a' && ch <= 'z') {
+                System.out.println("소문자입니다.");
+            } else if (ch >= 'A' && ch <= 'Z') {
+                System.out.println("대문자입니다.");
+            } else if (ch >= '0' && ch <= '9') {
+                System.out.println("숫자문자입니다.");
+            } else {
                 System.out.println("영문, 숫자 이외의 문자입니다.");
                 break;
             }
 
-            if (isDigit(ch)) {
-                System.out.println("숫자문자입니다.");
-            } else {
-                System.out.println(isUpperCase(ch) ? "대문자입니다." : "소문자입니다.");
-            }
+
         }
 
 
