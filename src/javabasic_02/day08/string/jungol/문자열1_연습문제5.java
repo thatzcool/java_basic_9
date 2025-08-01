dfd738599d0dbcc5e6226c28d4aee8125ab812a6
thatzcool <mycaptain0771@gmail.com>
2025-08-01 14:41:16 +0900

    jungol_string1_06

--- src/javabasic_02/day08/string/jungol/문자열1_연습문제5.java
+++ src/javabasic_02/day08/string/jungol/문자열1_연습문제5.java
@@ -6,6 +6,10 @@ public class 문자열1_연습문제5 {
     static Scanner in = new Scanner(System.in);
 
     public static void main(String[] args) {
+          String str = "우리나라 대한민국!";
+
+          System.out.println("우리나라 대한민국!\n위 문자열의 길이는 "+ str.length() + "입니다.");
+
 
 
     }
---
+++ src/javabasic_02/day08/string/jungol/문자열1_연습문제6.java
@@ -0,0 +1,33 @@
+package javabasic_02.day08.string.jungol;
+
+import java.util.Scanner;
+
+import static java.lang.Character.isDigit;
+import static java.lang.Character.isUpperCase;
+
+public class 문자열1_연습문제6 {
+    static Scanner in = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        while (true) {
+            String str = in.nextLine();
+            char ch = str.charAt(0);
+
+            if ((int) ch >= 123 || (int) ch <= 47) {
+                System.out.println("영문, 숫자 이외의 문자입니다.");
+                break;
+            }
+
+            if (isDigit(ch)) {
+                System.out.println("숫자문자입니다.");
+            } else {
+                System.out.println(isUpperCase(ch) ? "대문자입니다." : "소문자입니다.");
+            }
+        }
+
+
+    }
+
+}
+
