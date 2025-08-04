5d30556889c2d99c342520e0ae2737f7e7cef678
thatzcool <mycaptain0771@gmail.com>
2025-08-04 14:29:17 +0900

    methods_06

---
+++ src/javabasic_02/day09/methodbasic/ComputerMain.java
@@ -0,0 +1,25 @@
+package javabasic_02.day09.methodbasic;
+
+public class ComputerMain {
+
+    public static void main(String[] args) {
+        //Computer 객체 생성
+        Computer myCom = new Computer();
+
+        int result = 0;
+
+        result = myCom.totalSum(1, 2, 3);
+        System.out.println(result);
+
+        result = myCom.totalSum(1,2,3,4,5);
+        System.out.println(result);
+
+        int[] values = {1,2,3,4,5};
+        result = myCom.totalSum(values);
+
+        result = myCom.totalSum(new int[] {1,2,3,4,5,6,7,8,9,10});
+        System.out.println(result);
+
+    }
+
+}
