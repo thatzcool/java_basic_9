daaf1b0ad30b51624eae236eb71347cbda34f15a
thatzcool <mycaptain0771@gmail.com>
2025-08-07 14:33:35 +0900

    상속06

--- src/javabasic_02/day11/inheritance/sec07/exam03/Child.java
+++ src/javabasic_02/day11/inheritance/sec07/exam03/Child.java
@@ -2,8 +2,10 @@ package javabasic_02.day11.inheritance.sec07.exam03;
 
 public class Child extends Parent {
 	//필드 선언
-
+     public String field2;
 
 	//메소드 선언
-
+    public void method3() {
+        System.out.println("Child-method3()");
+    }
 }
--- src/javabasic_02/day11/inheritance/sec07/exam03/ChildExample.java
+++ src/javabasic_02/day11/inheritance/sec07/exam03/ChildExample.java
@@ -1,17 +1,26 @@
 package javabasic_02.day11.inheritance.sec07.exam03;
 
+
+
 public class ChildExample {
 	public static void main(String[] args) {
-		//객체 생성 및 자동 타입 변환
+		//부모 타입으로 자식객체를 생성하세요
+         Parent parent = new Child();
 
-		
+
 		//Parent 타입으로 필드와 메소드 사용
+          parent.field1 = "data1111";
+		  parent.method1();
+		  parent.method2();
+		  //parent.field2 = "data22222";
+		  //parent.method3();
 
-		
 		//강제 타입 변환
+		  Child child =(Child) parent;
+		  child.field2 = "data22222";
+		  child.method3();
 
-		
-		//Child 타입으로 필드와 메소드 사용
+
 
 	}
 }
