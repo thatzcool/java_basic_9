bd5add1ae65b65c7ba9d246571538fae6aabd05b
thatzcool <mycaptain0771@gmail.com>
2025-08-12 14:59:57 +0900

    exception_01

---
+++ src/java_advanced_01/day13/exceptionEx/ExceptionHandleingEx.java
@@ -0,0 +1,26 @@
+package java_advanced_01.day13.exceptionEx;
+
+public class ExceptionHandleingEx {
+
+
+    public static void main(String[] args) {
+        System.out.println("[프로그램 시작]");
+        try {
+            printLength("This is Exception Class");
+            printLength(null);
+        } catch (Exception e) {
+//            System.out.println(e.getMessage());
+//            e.printStackTrace();
+            System.out.println(e.toString());
+        } finally {
+            System.out.println("마무리 블럭");
+        }
+
+        System.out.println("프로그램 종료");
+    }
+
+    static void printLength(String param) {
+        int strLength = param.length();
+        System.out.println("전달받은 문자열의 길이" + strLength);
+    }
+}
