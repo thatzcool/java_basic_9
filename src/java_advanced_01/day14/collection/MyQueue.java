44767d44aa34f102e1b6ed4080d1ace5ca03faca
thatzcool <mycaptain0771@gmail.com>
2025-08-13 16:31:54 +0900

    collection_02

---
+++ src/java_advanced_01/day14/collection/MyQueue.java
@@ -0,0 +1,35 @@
+package java_advanced_01.day14.collection;
+
+import java.util.ArrayList;
+
+public class MyQueue<T> {
+      private ArrayList<T> queue = new ArrayList<T>();
+
+      public void enqueue(T item){
+          queue.add(item);
+      }
+
+      public T dequeue(){
+          if(queue.isEmpty()){
+              return  null;
+          }
+          return queue.remove(0);
+      }
+      public boolean isEmpty(){
+          return queue.isEmpty();
+      }
+
+    public static void main(String[] args) {
+          MyQueue<Integer>  mq = new MyQueue<>();
+          mq.enqueue(1);
+          mq.enqueue(10);
+          mq.enqueue(20);
+          mq.enqueue(30);
+
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+        System.out.println(mq.dequeue());
+    }
+
+}
---
+++ src/java_advanced_01/day14/collection/StackEx.java
@@ -0,0 +1,23 @@
+package java_advanced_01.day14.collection;
+
+import java.util.Stack;
+
+//Stack 구조는 데이터를 제한적으로 접근 할 수 있는 구조이다.  (한쪽 끝에서만 자료를 넣거나 뺄 수 있는 구조)
+//LIFO (Last In First Out)
+//구조가 단순해서, 구현이 쉽다. 데이터 저장/읽기 속도가 빠르다
+//데이터 최대 갯수를 미리 정해야 한다.  저장공간의 낭비가 발생할 수 있다.
+//자바에서 java.util.Stack 제공  push(), pop()
+public class StackEx {
+    public static void main(String[] args) {
+        Stack<Integer> stack_int = new Stack<>();
+        stack_int.push(1);
+        stack_int.push(2);
+        stack_int.push(3);
+
+        System.out.println(stack_int.pop());
+        System.out.println(stack_int.pop());
+        int data = stack_int.pop();
+        stack_int.push(data);
+    }
+
+}
