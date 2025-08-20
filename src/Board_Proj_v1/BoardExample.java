6d9800a2b9a2ad2bb1112b306e494de5152e1307
thatzcool <mycaptain0771@gmail.com>
2025-08-20 11:02:22 +0900

    board_v1_02

--- src/Board_Proj_v1/BoardExample.java
+++ src/Board_Proj_v1/BoardExample.java
@@ -1,69 +1,80 @@
 package Board_Proj_v1;
 
+import java.util.Map;
 import java.util.Scanner;
 
 //사용자가 현재 게시물목록 읽을 수 있다.
 //사용자가 메뉴를 선택하여 글을 생성, 생성한 글을 읽을 수도 있고, 삭제할 수도 있다.
 //사용자는 게시판을 프로그램을 종료할 수 있다.
 public class BoardExample {
+    static Scanner scanner = new Scanner(System.in);   //static 처리 => scanner 객체는 공용객체
+    private static final String menuNumberRegex = "[1-4]";
+    private static final String checkNumber = "[1-2]";
+    private static final String readOptionRegex = "[1-3]";
+    BoardManger boardManger;
 
+   public BoardExample(){}
+   public BoardExample(BoardManger boardManger){
+          this.boardManger = boardManger;
+    }
 
     // 현재 게시판의 전체 게시물 목록을 출력 기능
-    public void list(){
+    public void list() {
         String boardList = """
                        [게시물 목록]
                        --------------------------------------------------------
                        no      writer      date      title 
                        --------------------------------------------------------
-                        1      winter      2024.01.02  게시판에 오신것을 환영합니다.  
-                        2      winter      2024.01.01  올 겨울은 매우 춥네요.....  
-                           
-                """;
+                       """;
+        Map<String,Board> board= null;
+
+
+
+
         System.out.println(boardList);
         mainMenu();
     }
 
     //메인 메뉴
-    public void mainMenu(){
+    public void mainMenu() {
         String mainMenu = """
-                       --------------------------------------------------------
-                       메인메뉴 : 1. Create | 2. Read | 3. Clear | 4. Exit 
-                       --------------------------------------------------------
-                       메뉴선택 :
-                           
+                --------------------------------------------------------
+                메인메뉴 : 1. Create | 2. Read | 3. Clear | 4. Exit 
+                --------------------------------------------------------
                 """;
         System.out.println(mainMenu);
+
     }
 
     // 게시물 메인 메뉴 선택 기능
-    public void run(){
-        while(true){
+    public void run() {
+        while (true) {
             list();
-            Scanner scanner = new Scanner(System.in);
+            System.out.print("메뉴 선택 : ");
             String input = scanner.nextLine();  //외부에서 사용자가 입력한 데이터타입은 모두 문자열
 
-            if(input.equals("4")){
+            if (input.equals("4")) {
                 System.out.println("\n게시판 종료");
                 break;
             }
-            if(input.matches(menuNumberRegex)){
-                switch (input){
-                    case "1":  System.out.println("*** create() 메소드 실행됨");
-                    case "2":  System.out.println("*** read() 메소드 실행됨");
-                    case "3":  System.out.println("*** clear() 메소드 실행됨");
+            if (input.matches(menuNumberRegex)) {
+                switch (input) {
+                    case "1":
+                        System.out.println("*** create() 메소드 실행됨");
+                        break;
+                    case "2":
+                        System.out.println("*** read() 메소드 실행됨");
+                        break;
+                    case "3":
+                        System.out.println("*** clear() 메소드 실행됨");
+                        break;
                 }
-            }else {
+            } else {
                 System.out.println("[1-4]번 번호를 입력하세요");
             }
 
 
-
         }
-
-
-
-
-
 
 
     }
--- src/Board_Proj_v1/BoardMain.java
+++ src/Board_Proj_v1/BoardMain.java
@@ -2,7 +2,9 @@ package Board_Proj_v1;
 
 public class BoardMain {
     public static void main(String[] args) {
-        BoardExample boards = new BoardExample();
+        BoardManger boardManger = new BoardManger();
+        BoardExample boards = new BoardExample(boardManger);
+        // BoardExample boardExample = new BoardExample();
         boards.run();
     }
 
