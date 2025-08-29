0a6ef3634dbb6a112083e76a59a3464657a8ebb4
thatzcool <mycaptain0771@gmail.com>
2025-08-29 12:15:01 +0900

    tcpchatting

--- src/java_advanced/day24/jsonbased_chatting/ChatServer.java
+++ src/java_advanced/day24/jsonbased_chatting/ChatServer.java
@@ -3,19 +3,22 @@ package java_advanced.day24.jsonbased_chatting;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 //채팅서버 실행클래스가 하는 일에 대해 기술하세요
 //클라이언트의 연결 요청을 수락하고 통신용 SocketClient를 생성하는 역할을 한다.
-  import org.json.*;
+import org.json.*;
+
 public class ChatServer {
-   //3개의 필드
+    //3개의 필드
     ServerSocket serverSocket;  // 클라이언트의 연결 요청 수락
     ExecutorService threadPool = Executors.newFixedThreadPool(100); //100개의 클라이언트가 동시에 채팅할 수 있도록 운영하기 위해
-    Map<String,SocketClient> chatRoom = new Collections.synchronizedMap(new HashMap<>()); //통신용 SocketClient를 관리하는 동기화된 Map 컬렌션이다.
+    Map<String, SocketClient> chatRoom = new ConcurrentHashMap<>(new HashMap<>()); //통신용 SocketClient를 관리하는 동기화된 Map 컬렌션이다.
 
 
     //메소드 : 서버 시작 채팅서버가 시작할때 가장 먼저 호출되는 메소드
@@ -25,21 +28,85 @@ public class ChatServer {
         System.out.println("Server started");
 
         Thread thread = new Thread(
-                ()->{
-                    try{
-                        while(true){    //람다식이 하는일을 개발자가 아래와 같이 2가지를 지정함
+                () -> {
+                    try {
+                        while (true) {    //람다식이 하는일을 개발자가 아래와 같이 2가지를 지정함
                             Socket socket = serverSocket.accept();   //1. accpet()로 연결을 수락
-                            SocketClient client = new SocketClient();  //2. 통신용 SocketClient를 반복해서 생성한다.
+                            SocketClient client = new SocketClient(this,socket);  //2. 통신용 SocketClient를 반복해서 생성한다.
                         }
 
-                    } catch (Exception e) {
+                    } catch (IOException e) {
                         e.printStackTrace();
+                    } catch (Exception e) {
+
                     }
-
-
                 }
         );
         thread.start();
-
     }
+
+    // 메소드 : 클라이언트 연결시 SocketClient 생성 하고 Map에 추가하는 기능
+    public void addSocketClient(SocketClient socketClient) {
+        String key = socketClient.chatName + "@" + socketClient.clientIp; //클라이언트 정보를 이용하여 고유key생성
+        chatRoom.put(key, socketClient);
+        System.out.println("입장 > :" + key);
+        System.out.println("현재 채팅자 수: " + chatRoom.size() + "\n");
+    }
+
+
+    //메소드 : 클라이언트 연결 종료 시 SocketClient 제거
+    public void removetSocketClient(SocketClient socketClient) {
+        String key = socketClient.chatName + "@" + socketClient.clientIp;
+        chatRoom.remove(key);
+        System.out.println("퇴장> :" + key);
+        System.out.println("현재 채팅자 수: " + chatRoom.size() + "\n");
+    }
+
+    // 메시지 브로드캐스트 기능 : JSON메시지를 생성하여 채팅방에 있는 모든
+    //클라이언트에게 보내는 기능을 구현
+    public void sendToAll(SocketClient sender,String message) {
+        JSONObject root = new JSONObject();
+        root.put("clientIp", sender.clientIp);
+        root.put("chatName", sender.chatName);
+        root.put("message", message);
+        String json = root.toString();        // { "clientIp": "xxx.xxx.xx.11",
+                                              //   "chatName": "yumi",
+                                              //   "message" : "hello,오랫만이야" }
+        //Collections<SocketClient> 얻은 후 모든 SocketClient로 하여금
+        //send()로 JSON메시지를 보내게 한다.
+        Collection<SocketClient> clients = chatRoom.values();
+        for(SocketClient client : clients) {
+            if(client == sender) continue;
+            client.send(json);
+        }
+    }
+
+    //서버종료 : stop()
+    public void stop(){
+        try {
+            serverSocket.close();
+            threadPool.shutdown();
+            chatRoom.clear();
+            System.out.println("서버 종료");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/java_advanced/day24/jsonbased_chatting/SocketClient.java
+++ src/java_advanced/day24/jsonbased_chatting/SocketClient.java
@@ -1,4 +1,98 @@
 package java_advanced.day24.jsonbased_chatting;
 
+import org.json.JSONObject;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+//클라이언트와 1:1 로 통신하는 역할
 public class SocketClient {
+
+    //필요 객체 (필드)
+    ChatServer chatServer;  //ChatServer()의 메소드를 호출을 위해
+    Socket socket;          //연결을 끊을때 필요
+    DataInputStream dis;    //문자열을 읽을때
+    DataOutputStream dos;  //문자열을 보낼때
+    public String clientIp;     //클라이언트 Ip
+    String chatName;      //닉네임(대화명)
+
+    public SocketClient(ChatServer chatServer, Socket socket) {
+        try {
+            this.chatServer = chatServer;
+            this.socket = socket;
+            this.dis = new DataInputStream(socket.getInputStream());
+            this.dos = new DataOutputStream(socket.getOutputStream());
+            InetSocketAddress isa = (InetSocketAddress) socket.getRemoteSocketAddress();
+            this.clientIp = isa.getHostName();
+            receive();
+        } catch (IOException e) {
+           e.printStackTrace();
+        }
+    }
+
+    //메소드 : JSON 메세지 받기  : 클라이어트가 보낸 JSON 메시지를 읽는 업무를 수행한다.
+    // dis.readUTF()로 JSON을 읽고 JSONObject로 파싱해서 command값을 얻어내어 처리함
+    //command : incomming=> JSON에서 대화명을 읽는다. chatRoom에 SocketClient를 추가
+    //            message => 메세지를 읽고 연결되어 있는 모든 클라이언트들에게 메시지를 보낸다.
+    //          클라이언트가 채팅을 종료할 경우 dis.UTF()에서 IOException발생하므로 예외처리를 하여
+    // chatRoom에 저장되어 있는 SocketClient를 제거한다.
+
+    public void receive(){
+
+        chatServer.threadPool.execute(
+                () -> {
+
+                    try {
+                        while (true) {
+
+                            String receiveJson = dis.readUTF();
+
+                            JSONObject obj = new JSONObject(receiveJson);
+                            String command = obj.getString("command");
+
+                            switch (command) {
+                                case "incomming" :
+                                    this.chatName = obj.getString("data");
+                                    chatServer.sendToAll(this,"입장하셨습니다.");
+                                    chatServer.addSocketClient(this);
+                                    break;
+                                case "message" :
+                                    String message = obj.getString("data");
+                                    chatServer.sendToAll(this,message);
+                                    break;
+                            }
+
+                        }
+                    } catch (IOException e) {
+                       chatServer.sendToAll(this,"퇴장하셨습니다.");
+                       chatServer.removetSocketClient(this);
+                    }
+
+
+                }
+
+        );
+    }
+
+    //메소드 : JSON 보내기  연결된 클라이언트로 JSON메세지를 보내는 기능
+    public void send(String json){
+        try {
+            dos.writeUTF(json);
+            dos.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+// 메소드 연결 종료
+    public void close(){
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
